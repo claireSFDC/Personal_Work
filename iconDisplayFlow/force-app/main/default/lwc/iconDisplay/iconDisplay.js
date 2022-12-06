3acec13bf44b7b78b30f8e087de13df1879e4af4
claireSFDC <62670451+claireSFDC@users.noreply.github.com>
2022-12-06 12:45:44 -0500

    Add files via upload

---
+++ iconDisplayFlow/force-app/main/default/lwc/iconDisplay/iconDisplay.js
@@ -0,0 +1,29 @@
+import { LightningElement , api } from 'lwc';
+
+export default class IconDisplay extends LightningElement {
+    @api iconType;
+    @api iconName;
+    @api foregroundColor;
+    @api backgroundColor;
+
+    get iconTypeName(){
+        return this.iconType + ":" + this.iconName;
+    }
+
+    get myIconClass(){
+        return !this.foregroundColor || !this.backgroundColor ? '' : 'my-icon'
+    }
+    //both foregroundColor and backgroundColor are required to set custom color. 
+
+    renderedCallback() { 
+        this.initCSSVariables();
+    }
+
+    initCSSVariables() {
+        var css = this.template.host.style;
+        css.setProperty('--backgroundColor', this.backgroundColor);
+        css.setProperty('--foregroundColor', this.foregroundColor);
+    }
+    //CSS function from Force Panda: 
+    //https://forcepanda.wordpress.com/2021/07/27/how-to-dynamically-set-custom-css-properties-in-lwc/
+}
